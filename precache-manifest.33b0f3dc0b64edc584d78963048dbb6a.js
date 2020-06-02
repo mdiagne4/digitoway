07b196c2b1ea34e50c3a701778c3888579f9d279
Mouhamadou Diagne <dmouha@hotmail.fr>
2020-06-02 21:10:40 +0200

    Updates

---
+++ precache-manifest.33b0f3dc0b64edc584d78963048dbb6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5827894ee679a4a0a3a0348411c5d4",
+    "url": "/mdiagne4/digitoway.git/index.html"
+  },
+  {
+    "revision": "9d2023ba1f4d0e71c76a",
+    "url": "/mdiagne4/digitoway.git/static/css/main.c0710856.chunk.css"
+  },
+  {
+    "revision": "32368fdf58ae97956ff1",
+    "url": "/mdiagne4/digitoway.git/static/js/2.743dce36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mdiagne4/digitoway.git/static/js/2.743dce36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2023ba1f4d0e71c76a",
+    "url": "/mdiagne4/digitoway.git/static/js/main.502e7059.chunk.js"
+  },
+  {
+    "revision": "1458e12ca50520b17919",
+    "url": "/mdiagne4/digitoway.git/static/js/runtime-main.54ca75cf.js"
+  }
+]);
